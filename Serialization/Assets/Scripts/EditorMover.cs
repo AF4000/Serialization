0e9fb5a8d3647c84377a7355fb7dd5e21ad59a73
AF4000 <fily4000@gmail.com>
2025-08-25 20:24:43 +0300

    25.08

---
+++ Serialization/Assets/Scripts/EditorMover.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))] // у объекта компонент PositionSaver
+    public class EditorMover : MonoBehaviour //наследует от монобихевер: позицию объекта, задержку записи и продолжительность
+	{
+		private PositionSaver _save; //Поле сохранения ссылки на компонент
+        private float _currentDelay; //Время задержки между записями позиции
+
+        //todo comment: Что произойдёт, если _delay > _duration?
+		//изменение положения объекта не будут сохранены
+        private float _delay = 0.5f; // частота кадра, интервал между записями
+        private float _duration = 5f;//общее время записи
+
+        private void Start()
+		{
+            //todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+            //в Update эта операция будет часто повторяться , при каждом сохранении изменения положения, поэтому стираем перед началом записи движения
+            _save = GetComponent<PositionSaver>(); //ищется компонент PositionSaver у объекта
+            _save.Records.Clear();//чистка записей перед началом
+        }
+
+		private void Update() //в каждом кадре уменьшается общее время записи и когда оно станет меньше 0, отключится сохранение и вылезет сообщение,что все завершено
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+
+            //todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+            //_delay интервал времени, частота кадра, она не должна меняться. А в этой строке она получается будет уменьшаться со временем
+            _currentDelay -= Time.deltaTime;//счетчик времени следующей записи
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;//если меньше или равно 0, то равна частоте кадра
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+					//todo comment: Для чего сохраняется значение игрового времени?
+					//для расчета дельты,игровое время связанно с временем записи и определением пришло ли время для конкретной записи относительно текущего времени?
+					Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Serialization/Assets/Scripts/PositionSaver.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class PositionSaver : MonoBehaviour //класс хранит в себе позиции объекта связан с реплей муви
+	{
+		public struct Data
+		{
+			public Vector3 Position;
+			public float Time;
+		}
+
+		private TextAsset _json;//ассет по позициям объекта
+
+		public List<Data> Records { get; private set; } //сохраненные данные по позициям объекта в файле
+
+        private void Awake()
+		{
+			//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+			//в условии идет проверка наличия ассета по позициям объекта , если его нет отключаем объект, сообщаем о ошибке, закрываем метод. если не выйти то если ассет будет отсутствовать, программа будет дальше делать десериализацию и выдаст ошибку
+			if (_json == null)
+			{
+				gameObject.SetActive(false);
+				Debug.LogError("Please, create TextAsset and add in field _json");
+				return;
+			}
+			
+			JsonUtility.FromJsonOverwrite(_json.text, this);//передача данных из ассета в текущий объект
+			//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+			//если данных нет, то создается новый список.Если нет ассета, то мы закрыли предыдущий метод передачи данных о положении объекта и начали записывать новые данные. Без этого новые данные записываться не будут.
+			if (Records == null)
+				Records = new List<Data>(10);
+		}
+
+		private void OnDrawGizmos()
+		{
+			//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
+			//если нет данных об объекте, то закрываем метод. Если данные есть, то по этим позициям рисуем сферы и получаем отображение траектории цветом
+			if (Records == null || Records.Count == 0) return;
+			var data = Records;
+			var prev = data[0].Position;
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(prev, 0.3f);
+			//todo comment: Почему итерация начинается не с нулевого элемента?
+			//нулевое значение , это базовое значение до начала изменения положения объекта
+			for (int i = 1; i < data.Count; i++)
+			{
+				var curr = data[i].Position;
+				Gizmos.DrawWireSphere(curr, 0.3f);
+				Gizmos.DrawLine(prev, curr);
+				prev = curr;
+			}
+		}
+		
+#if UNITY_EDITOR
+		[ContextMenu("Create File")]//создание файла в папке объекта
+		private void CreateFile()
+		{
+			//todo comment: Что происходит в этой строке?
+			//создание самого файла
+			var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
+			//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав)
+			//закрытие файла и обновление данных ассета (при закрытии эти данные автоматически обновляются, на уроке говорили)
+			stream.Dispose();
+			UnityEditor.AssetDatabase.Refresh();
+			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
+			//После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
+			var guids = UnityEditor.AssetDatabase.FindAssets("t:TextAsset");
+			foreach (var guid in guids)
+			{
+				//Этой командой можно получить путь к ассету через его гуид
+				var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+				//Этой командой можно загрузить сам ассет
+				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+				//todo comment: Для чего нужны эти проверки?
+				//проверяем имя файла и он становится ассетом джейсон, сохраняем изменения
+				if(asset != null && asset.name == "Path")
+				{
+					_json = asset;
+					UnityEditor.EditorUtility.SetDirty(this);
+					UnityEditor.AssetDatabase.SaveAssets();
+					UnityEditor.AssetDatabase.Refresh();
+					//todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
+					//у нас уже есть ассет, дальше искать уже ничего не нужно
+					return;
+				}
+			}
+		}
+
+		private void OnDestroy()
+		{
+			//todo logic...
+		}
+#endif
+	}
+}
